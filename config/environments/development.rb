b597c5cc1108875940a4ba492d199120cb69fe23
Konrad Zdzieba <konrad.zdzieba@croscomsoftware.com>
2018-10-30 21:09:17 +0100

    Added devise gem and User model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
@@ -51,6 +53,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
